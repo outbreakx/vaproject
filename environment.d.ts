26083ab8f5da02025a0a2395368bad87f29e3c43
Leonardo Igor de Oliveira Cavalcante <bufige@yahoo.com>
2022-01-13 14:43:26 -0300

    fixes build and its path issues

--- environment.d.ts
+++ environment.d.ts
@@ -3,7 +3,7 @@ declare global {
 		interface ProcessEnv {
 			NODE_ENV: 'development' | 'production';
 			SERVER_URL: string;
-			SERVER_PORT: number;
+			SERVER_PORT: string;
 			MONGO_URL: string;
 			MONGO_COLLECTION: string;
 		}
--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,7 @@
 import dotenv from 'dotenv';
 
 dotenv.config({
-	path: process.env.NODE_ENV === "test" ? ".env.testing" : ".env"
+	path: process.env.NODE_ENV === "development" ? ".env" : ".env.production"
 });
 
 import { app } from "./app";
